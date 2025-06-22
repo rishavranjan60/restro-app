ea81aa478345419f01662fccf888bfd8946aac33
rishavranjan60 <rishavranjan7744@gmail.com>
2025-06-22 04:25:38 +0200

    Prepare CRA client for Render deploy

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,14 +1,15 @@
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 export const placeOrder = async (order: any) => {
-    const res = await fetch("http://localhost:5000/api/orders", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(order),
-    });
-  
-    if (!res.ok) {
-      throw new Error("Order failed");
-    }
-  
-    return res.json();
-  };
-  
+  const res = await fetch(`${API_BASE_URL}/orders`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(order),
+  });
+
+  if (!res.ok) {
+    throw new Error("Order failed");
+  }
+
+  return res.json();
+};
