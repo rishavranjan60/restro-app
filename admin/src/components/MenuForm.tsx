9d86252ede95add1d899cfb5364c8bad87f46113
rishavranjan60 <rishavranjan7744@gmail.com>
2025-06-26 19:09:14 +0200

    fixed admin api and .env in .gitignore

--- admin/src/components/MenuForm.tsx
+++ admin/src/components/MenuForm.tsx
@@ -137,7 +137,7 @@ const MenuForm: React.FC = () => {
 
   return (
     <div className="p-6">
-      <h2 className="text-2xl font-bold mb-4">📋 Manage Menu</h2>
+      <h2 className="text-2xl font-bold mb-4">Manage Menu</h2>
 
       {/* FORM */}
       <div className="bg-gray-800 p-4 rounded-lg mb-6">
